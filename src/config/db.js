6f3404277e429abeaadc1d38a3c8553a021bc6aa
Leidi <leidineiacruz1@gmail.com>
2026-05-21 13:25:14 -0400

    teste banco

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+export const conectarBanco = async () => {
+  try {
+    const URL = process.env.MONGODB_URI;
+    
+    if (!URL) {
+      throw new Error("A variável MONGODB_URI não foi definida no arquivo .env");
+    }
+
+    await mongoose.connect(URL);
+    console.log("Conexão com o MongoDB Atlas realizada com sucesso! 🚀");
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error.message);
+    process.exit(1); 
+  }
+};
