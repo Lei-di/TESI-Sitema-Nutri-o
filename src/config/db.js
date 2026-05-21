c56b1866ec2d558980d47512bc505cf154cdd21f
Leidi <leidineiacruz1@gmail.com>
2026-05-21 13:51:19 -0400

    teste bd

--- src/config/db.js
+++ src/config/db.js
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 export const conectarBanco = async () => {
-  try {
-    const URL = process.env.MONGODB_URI;
-    
-    if (!URL) {
-      throw new Error("A variável MONGODB_URI não foi definida no arquivo .env");
-    }
+    try {
+        const URL = process.env.MONGODB_URI;
 
-    await mongoose.connect(URL);
-    console.log("Conexão com o MongoDB Atlas realizada com sucesso! 🚀");
-  } catch (error) {
-    console.error("Erro ao conectar ao banco de dados:", error.message);
-    process.exit(1); 
-  }
+        if (!URL) {
+            throw new Error("A variável MONGODB_URI não foi definida no arquivo .env");
+        }
+
+        await mongoose.connect(URL);
+        console.log("Conexão com o MongoDB Atlas realizada com sucesso! 🚀");
+    } catch (error) {
+        console.error("Erro ao conectar ao banco de dados:", error.message);
+        process.exit(1);
+    }
 };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { conectarBanco } from "./config/db.js";
+
+console.log("Iniciando o Sistema de Nutrição... 🥗");
+
+//conexão com mongodb atlas
+conectarBanco();
