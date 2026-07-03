f0247644cccd8fa2386fa8d1aa0b2891781f022b
Leidi <leidineiacruz1@gmail.com>
2026-07-03 11:43:36 -0400

    criptografia corrigida

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -10,25 +10,18 @@ const usuarioSchema = new mongoose.Schema({
 
 usuarioSchema.pre('save', async function () {
     const usuario = this;
-    console.log('>>> Hook pre-save foi chamado');
 
-    // Se a senha não foi modificada, encerra a função sem fazer nada
     if (!usuario.isModified('senha')) {
         return; 
     }
 
     try {
-        // Gera o salt e o hash de forma assíncrona (sem callbacks)
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(usuario.senha, salt);
-        
-        console.log('>>> Hash gerado:', hash);
 
-        // Atribui a senha criptografada ao usuário
         usuario.senha = hash;
         
     } catch (err) {
-        // Em caso de erro na criptografia, repassa para o Mongoose tratar
         throw err;
     }
 });
