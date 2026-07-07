9b16272c112549f70601e16c46b5946653495fb6
Leidi <leidineiacruz1@gmail.com>
2026-07-07 14:57:48 -0400

    arquivo insomnia

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,3 +1,4 @@
+import { autenticarToken } from "../middlewares/authMiddleware.js";
 import { Router } from "express";
 import { criarUsuario, listarUsuarios, buscarUsuarioPorId, atualizarUsuario, deletarUsuario } from "../controllers/UsuarioController.js";
 import validarUsuario from "../middlewares/validarUsuario.js"; 
@@ -5,9 +6,9 @@ import validarUsuario from "../middlewares/validarUsuario.js";
 const router = Router();
 
 router.post("/", validarUsuario, criarUsuario);
-router.get("/", listarUsuarios);
-router.get("/:id", buscarUsuarioPorId);
-router.put("/:id", atualizarUsuario);
-router.delete("/:id", deletarUsuario);
+router.get("/", autenticarToken, listarUsuarios);
+router.get("/:id", autenticarToken, buscarUsuarioPorId);
+router.put("/:id", autenticarToken, atualizarUsuario);
+router.delete("/:id", autenticarToken, deletarUsuario);
 
 export { router as usuarioRoutes };
